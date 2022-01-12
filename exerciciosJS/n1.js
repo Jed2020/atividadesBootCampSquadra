d2014e88977f581d8a55d786ab89d1e46934f082
emerson <69053988+Jed2020@users.noreply.github.com>
2022-01-12 18:54:53 -0300

    create n1 at exercicioJS

---
+++ exerciciosJS/n1.js
@@ -0,0 +1,15 @@
+/*1.(WHILE) Crie o array do MODELO_01. Percorra o array e procure qual o menor número que existe dentro do array. 
+Resultado esperado: O passageiro mais novo do ônibus tem 0 anos e está na cadeira 6.
+Ônibus = [2, 8, 9, 5, 4, 6, 0, 7, 1, 3]*/
+
+
+const bus = [2, 8, 9, 5, 4, 6, 0, 7, 1, 3];
+let i = 0;
+let age = "";
+let index = bus.indexOf(0);
+
+while (bus[i]) {
+  age = [bus.length = 0];
+  i++;
+}
+console.log("O passageiro mais novo do ônibus tem " + age + " anos e está na cadeira " +  index + ".");
